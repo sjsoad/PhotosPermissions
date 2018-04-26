1c173cde46e090518d19874f1c8cc0a3527bdbe5
Sergey Kostyan <>
2018-04-26 21:36:04 +0300

    move to open

---
+++ PhotosPermissions/PhotosPermissions.h
@@ -0,0 +1,19 @@
+//
+//  PhotosPermissions.h
+//  PhotosPermissions
+//
+//  Created by Sergey Kostyan on 24.04.2018.
+//  Copyright © 2018 Sergey Kostyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotosPermissions.
+FOUNDATION_EXPORT double PhotosPermissionsVersionNumber;
+
+//! Project version string for PhotosPermissions.
+FOUNDATION_EXPORT const unsigned char PhotosPermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotosPermissions/PublicHeader.h>
+
+
---
+++ PhotosPermissions/Sources/PhotoLibraryPermissions.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoLibraryPermissions.swift
+//  SKUtilsSwift
+//
+//  Created by Sergey Kostyan on 01.10.16.
+//  Copyright © 2016 Sergey Kostyan. All rights reserved.
+//
+
+import UIKit
+import Photos
+import SKServicePermissions
+
+open class PhotoLibraryPermissions: NSObject {
+    
+}
+
+// MARL: - ServicePermissions -
+
+extension PhotoLibraryPermissions: ServicePermissions {
+    
+    public typealias PermissionsState = PHAuthorizationStatus
+    
+    public func requestPermissions(handler: @escaping (PermissionsState) -> Void) {
+        PHPhotoLibrary.requestAuthorization { [weak self] _ in
+            guard let strongSelf = self else { return }
+            handler(strongSelf.permissionsState())
+        }
+    }
+    
+    public func permissionsState() -> PermissionsState {
+        return PHPhotoLibrary.authorizationStatus()
+    }
+    
+}
